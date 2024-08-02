50a1225d9dc619310e9cb14963b4987aeccfeffc
StructurologyForensics <forensics@structurology.com>
2024-08-02 11:51:14 -0500

    Add files via upload

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import '$lib/app.css';
+</script>
+<nav class='top'>
+    <a href='/'><p>Jarvis</p></a>
+    <a href='/asce'><p>ASCE</p></a>
+    <a href='/googleMaps'><p>Google Maps</p></a>
+</nav>
+<hr class='top'/>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<h1 class='page-title red'>Jarvis</h1>
+
+<div class='page-box'>
+    <p class='red'>Welcome to Jarvis! Please feel free to use any of the tools located on this page!
+    </p>
+</div>
+
+<div class='page-box'>
+    <h1 class='red'>Tools</h1>
+    <a href='/googleMaps'>
+        <p>Satellite and Street View Images</p>
+    </a>
+    <a href='/asce'>
+        <p>ASCE PDF Downloader</p>
+    </a>
+</div>
